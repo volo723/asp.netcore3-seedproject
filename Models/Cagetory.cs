b88e2d6df99e72cedf99d57a3e66d095d16a715c
nastia723 <nastia723@outlook.com>
2020-05-21 21:06:49 +1000

    init

---
+++ Models/Cagetory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Attendance.Models
+{
+    public class Cagetory
+    {
+        public int CategoryID { get; set; }
+
+        public string CategoryCode { get; set; }
+        public string CategoryDescription { get; set; }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,1 @@
+#ASP .NET Core3 Razor Seed Project
